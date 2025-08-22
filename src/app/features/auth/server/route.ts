dfe60f7dd46f2ebb8bbf31e844d7a4d9190a1fd2
Atila <atilahunesco@gmail.com>
2025-08-21 21:09:06 -0300

    security: forgot to add the same logic for the login.

--- src/app/features/auth/server/route.ts
+++ src/app/features/auth/server/route.ts
@@ -9,14 +9,23 @@ import { AUTH_COOKIE } from '../constants'
 const app = new Hono()
 .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } =  c.req.valid("json")
-    console.log({ email, password })
+    const { account } = await createAdminClient()
+    const session = await account.createEmailPasswordSession(email, password)
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60*60*24*30
+    })
     
-    return c.json({ email, password })
+    return c.json({ success: true })
 })
 .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } =  c.req.valid("json")
     const { account } = await createAdminClient()
-    const user = await account.create(
+    await account.create(
         ID.unique(),
         email,
         password,
@@ -32,7 +41,7 @@ const app = new Hono()
         maxAge: 60*60*24*30
     })
 
-    return c.json({ data: user })
+    return c.json({ success: true })
 })
 
 export default app
